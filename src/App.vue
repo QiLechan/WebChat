6dc40c0c21bf37fd52ea4502fba426e377139e91
QiLechan <qilechan@outlook.com>
2023-10-13 20:43:12 +0800

    message输入

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 import Message from './components/message.vue'
 </script>
 
