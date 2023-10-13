f9cd039e80e745962af933042e3a0c1605beb9c5
QiLechan <qilechan@outlook.com>
2023-10-13 20:08:33 +0800

    first commit

---
+++ src/components/message.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+defineProps<{
+  message: string
+}>()
+</script>
+
+<template>
+<p>Message is: {{ message }}</p>
+<input v-model="message" placeholder="edit me" />
+</template>
