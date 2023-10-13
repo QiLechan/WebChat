2d2b41f97a679e4f498d54e512a66bd3970afcde
QiLechan <qilechan@outlook.com>
2023-10-13 21:28:52 +0800

    添加图片

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import Message from './components/message.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="./assets/logo.png" width="125" height="125" />
   </header>
 
   <main>
